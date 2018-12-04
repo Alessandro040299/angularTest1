fd4f5bb4be99d5d5ba7d1f6131acb96a46311875
Alessandro <olivaalessandro9@gmail.com>
2018-12-04 17:14:08 +0100

    salvataggio2

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, } from '@angular/core';
+import {Recipe} from "./recipe.model";
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe(
+      "amatriciana",
+      "guanciale e sugo",
+      "http://finedininglovers-it.cdn.crosscast-system.com/BlogPost/l_3494_Amatriciana.jpg"),
+
+      new Recipe(
+        "carbonara",
+        "guanciale e uovo",
+        "https://www.gustissimo.it/articoli/ricette/pasta-salumi/spaghetti-alla-carbonara.jpg")
+
+  ];
 
   constructor() { }
 
